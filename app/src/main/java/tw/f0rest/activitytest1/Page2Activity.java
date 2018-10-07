64bf980234eddd2e78f06f245d455c6fd8ea2b47
F0restraven <xiii9301@gmail.com>
2018-10-07 10:50:55 +0800

    ActivityLifeCircle

--- app/src/main/java/tw/f0rest/activitytest1/Page2Activity.java
+++ app/src/main/java/tw/f0rest/activitytest1/Page2Activity.java
@@ -1,7 +1,9 @@
 package tw.f0rest.activitytest1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Page2Activity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Page2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+    }
+
+    public void go2(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
